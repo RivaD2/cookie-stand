84446280b2920d1d2e8c0189bd245c307776d0e9
RivaD2 <rivkadavidowski@gmail.com>
2020-07-15 16:26:14 -0700

    added additional CSS for index.html and sales.html

--- app.js
+++ app.js
@@ -20,22 +20,31 @@ function Store (name, minCust, maxCust,avgCustomerSale) {
   this.storeHrs = storeHrs;  
 }
 
-Store.prototype.render= function() {                   // Start here for table creation and add html element if it doesn't exist (that is step 2)
-  var mainTable = document.getElementById("mainTable"); // We get a reference to the table here
-  var storeTableRow = document.createElement('tr');   // create a table row because we need one ROW FOR EVERY STORE --(working inward toward child elements)
+//** Need to add totals to table and times at top */
+Store.prototype.render= function() {// Start here for table creation and add html element if it doesn't exist (that is step 2)
+  var mainTable = document.getElementById('mainTable'); // We get a reference to the table here
+  var storeTableRow = document.createElement('tr');// create a table row because we need one ROW FOR EVERY STORE --(working inward toward child elements)
   //create table data for store name
   var storeNameTableData = document.createElement('td'); // EVERY ROW HAS TABLE DATA so we had to reference to the td 
   // set the content of the table data
-  storeNameTableData.textContent = this.name; 
-  //added table data to the ROW
+  storeNameTableData.textContent = this.name; //this is not a method, we are assigning a property 
+  //added table data to the ROW object and by passing it in
   storeTableRow.appendChild(storeNameTableData);
+
 
   //* IF I WANT TO CREATE ANOTHER TABLE, I WOULD USE ANOTHER RENDER FUNCTION(use name like employeeTable)
 
-// Loop through storeHrs and create table data for each one
-// I will need to calc cookiesSold (create var) and set it to text content of table data
-// Append table data to row
+  //TO FINISH TABLE
+  //add global footerFunction for TOTAL for all stores
+  //add global headerFunction to Store Hours display
+  // function tableFooter() {
+  // }
 
+
+
+  // Loop through storeHrs and create table data for each one
+  // I will need to calc cookiesSold (create var) and set it to text content of table data
+  // Append table data to row
 
   for (var i = 0; i < this.storeHrs.length; i++) {
     var custThisHour = this.getRandomNum(this.minCust,this.maxCust); 
@@ -49,24 +58,28 @@ Store.prototype.render= function() {                   // Start here for table c
 
   // added the row to the table
   mainTable.appendChild(storeTableRow);
-}
+};
 
 
-Store.prototype.getRandomNum = function(min, max) {  // Took getRandomNum and we turned it into a method
-  min = Math.ceil(min);                              // The same code still works in the method
-  max = Math.floor(max);                             // THe only thing I had to change was the function to the method, and line34 to this.getRandom
+
+Store.prototype.getRandomNum = function(min, max) { // Took getRandomNum and we turned it into a method
+  min = Math.ceil(min); // The same code still works in the method
+  max = Math.floor(max);// THe only thing I had to change was the function to the method, and line34 to this.getRandom
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
+//math.random MDN reference above used with prototype method
+
 
 
 //SECOND, 5 DIFFERENT INSTANCES OF THE STORE OBJECT ARE CREATED
 // Below each instance are the render method calls/function calls for each location using listOfTimes
 
-var seattleStore = new Store('Seattle',23,65,6.3);  // new Store(arguments)
-var tokyoStore = new Store('Tokyo',3,24,1.2);   // new variables aren't necessary 
+var seattleStore = new Store('Seattle',23,65,6.3); // new Store(arguments)
+var tokyoStore = new Store('Tokyo',3,24,1.2);// new variables aren't necessary 
 var dubaiStore = new Store('Dubai',11,38,3.7);
 var parisStore = new Store('Paris',20,38,2.3);
 var limaStore = new Store('Lima',2,16,4.7);
+
 
 //seattleStore.listOfTimes();
 seattleStore.render(); // render method still holds the functionality that listOfTimes function had
@@ -74,7 +87,6 @@ tokyoStore.render();
 dubaiStore.render();
 parisStore.render();
 limaStore.render();
-
 
 
 
